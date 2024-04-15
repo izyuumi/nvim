3352817a7d62da593cd2769e31f861394337abb1
Yumi Izumi <me@yumiizumi.com>
2024-04-15 19:50:16 -0400

    feat: change where sessions are saved in directory

--- after/plugin/session.lua
+++ after/plugin/session.lua
@@ -1,7 +1,7 @@
 local Path = require('plenary.path')
 local config = require('session_manager.config')
 require('session_manager').setup({
-  sessions_dir = Path:new(vim.loop.cwd(), "sessions"), -- The directory where the session files will be saved.
+  sessions_dir = Path:new(vim.loop.cwd(), "nvim_sessions"), -- The directory where the session files will be saved.
   session_filename_to_dir = session_filename_to_dir, -- Function that replaces symbols into separators and colons to transform filename into a session directory.
   dir_to_session_filename = dir_to_session_filename, -- Function that replaces separators and colons into special symbols to transform session directory into a filename. Should use `vim.loop.cwd()` if the passed `dir` is `nil`.
   autoload_mode = config.AutoloadMode.CurrentDir, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
